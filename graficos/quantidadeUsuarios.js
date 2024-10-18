34674f164ace1197c55315b0b7d3e1d65f881dae
WiliamdaRochaArantes <106930799+WiliamdaRochaArantes@users.noreply.github.com>
2024-10-18 09:55:05 -0300

    Create quantidadeUsuarios.js

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,29 @@
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar'
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const laytout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, laytout)
+}
+
+quantidadeUsuariosPorRede()
